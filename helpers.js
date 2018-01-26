72c589b3be1d9abfd88f0fa006587acf7a8418a9
Simon B <simon.bohlin@gmail.com>
2018-01-26 03:08:47 +0100

    node helper code documentation

--- helpers.js
+++ helpers.js
@@ -1,6 +1,9 @@
 // helpers
 
 module.exports = {
+	//
+	// Return a javascript document
+	//
 	js: function(res, string, status) {
 		res.writeHead(status || 200, {
 			'Content-Type': 'application/javascript'
@@ -9,6 +12,9 @@ module.exports = {
 		res.end();
 	},
 
+	//
+	// Return a html document
+	//
 	html: function(res, string, status) {
 		res.writeHead(status || 200, {
 			'Content-Type': 'text/html'
@@ -17,6 +23,9 @@ module.exports = {
 		res.end();
 	},
 
+	//
+	// Redirect the browser
+	//
 	redirect: function(res, url, status) {
 		res.writeHead(status || 302, {
 			'Location': url,
@@ -26,15 +35,25 @@ module.exports = {
 		res.end();
 	},
 
+	//
+	// Get the next value, calculated from the previous one
+	//
 	next_value: (value) => value + Math.floor(2 * Math.random()) - 1,
 	// next_value: function(value) { return value + Math.floor(2 * Math.random()) - 1 },
 
+	//
+	// Create a HTML document with headlines
+	// The friendly background color is supposedly liked by both men and women
+	//
 	friendly_page: function(subject, tagline, content) {
 		style = 'background:#7fdba3; zoom: 2; margin:3em; font-family:roboto,arial;';
 		return '<body style="' + style + '"><h3>' +
 			subject + '</h3><h4>' + tagline + '</h4>' + content + '</body>';
 	},
 
+	//
+	// Return a JSON document
+	//
 	json: function(res, structure, status, mimetype) {
 		mime = mimetype ||  'application/json';
 		res.writeHead(status ||  200, {
