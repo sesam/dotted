47d5824041e4bfceb47079201e7c91f07a6bac34
Simon B <simon.bohlin@gmail.com>
2018-01-28 01:44:35 +0100

    /deployed: detect when newly deployed server is ready

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
+var http = require('http');
 const args = require('yargs').argv;
 var port = args.port || 5001;
-var next_port = 5001 + (port - 5000) % 998;
+var next_port = (port) => 5001 + (port - 5000) % 998;
+var ip = '127.0.0.1';
 var value = args.value || 1000 * Math.random();
 var counter = args.counter ||  0;
 var failcounter = args.failcounter ||  0;
 
 var helpers = require('./helpers');
-helpers.save_ports('.dotted', port, next_port);
 var indexhtml = helpers.read_or_default('index.html', '<marquee>PAGE MISSING</marquee>');
 var indexjs = helpers.read_or_default('js/index.js', 'alert("Try again later")');
+
+//
+// Version tracking
+//
 var dotted = () => ({
 	major: helpers.read_or_default('.dotted/major_version', null),
 	tag: helpers.read_or_default('.dotted/tracking_tag', null),
@@ -18,7 +23,40 @@ var dotted = () => ({
 var current = dotted();
 var deployed = current;
 var target = current;
-var redeployed = () => ({ running: current, last: deployed = dotted() });
+var redeployed = () => ({ running: current, last: deployed = dotted(check_target()) });
+function take_target(res) {
+	res.setEncoding('utf8');
+	let body = '';
+	res.on('data', (data) => (body += data));
+	res.on('end', () => {
+		console.log(body);
+		// todo: use params from response
+		js = JSON.parse(body);
+		target = deployed.clone(); // probably exactly what we want
+		target.port = js.port; // but let's make sure
+		target.major = js.major;
+		target.tag = js.tag;
+	});
+}
+function check_target() {
+	if (current.major >= deployed.major) {
+		return console.log('nothing new ' +
+			JSON.stringify(deployed) + ' == ' + JSON.stringify(current));
+	}
+	console.log('checking deploy ' + JSON.stringify(deployed));
+	var deployed_url = 'http://' + ip + ':' + deployed.port + '/status';
+	var req = http.get(deployed_url, take_target);
+	req.on('socket', (socket) => {
+		socket.setTimeout(800);
+		socket.on('timeout', () => req.abort());
+		console.log('0down: check_target timeout - retrying in 500 ');
+		if (current.major >= deployed.major) setTimeout(check_target, 500);
+	});
+	req.on('error', function(err) {
+		console.log('0down: check_target error - retrying in 500 ' + err.code);
+		if (current.major >= deployed.major) setTimeout(check_target, 500);
+	});
+}
 
 // Not serverless yet - so failures and some offline support for better looks
 var offlinehtml = helpers.read_or_default('offline.html', '<marquee>PAGE MISSING</marquee>');
@@ -79,7 +117,7 @@ var handlers = {
 		(req, res) => helpers.redirect(res, favicon_url),
 }
 
-var http = require('http');
+// start http server
 var server = http.createServer(function(req, res) {
 	if (handler = handlers[req.url]) {
 		handler(req, res);
@@ -88,10 +126,20 @@ var server = http.createServer(function(req, res) {
 		failhandler(res);
 	}
 });
-
-var timeout = 5000/3.0 * (3.0 + Math.random());
-console.log('0down: waiting ' + Math.round(timeout/1000.0, 2) + 's -- params ', process.argv);
-setTimeout(function() {
-	server.listen(port);
+server.on('listening', () => {
 	console.log('0down: serving on port ' + port);
-}, timeout);
+	helpers.save_ports('.dotted', port, next_port(port));
+});
+server.on('error', (err) => {
+	console.log('error: ' + err);
+	port = next_port(port);
+	console.log('since something failed, we try next port = ' + port);
+	start_server();
+});
+function start_server() {
+	console.log('trying to listen on' + port);
+	server.listen(port);
+}
+var timeout = 5/3.0 * (3.0 + Math.random());
+console.log('0down: waiting ' + Math.round(timeout, 1) + 's -- params ', process.argv);
+setTimeout(start_server, timeout * 1000.0);
