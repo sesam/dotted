dc5bce5a98bd92a6ad1aaafb1e3bf76149e881e1
Simon B <simon.bohlin@gmail.com>
2018-02-07 00:30:47 +0100

    cleanup

--- js/index.js
+++ js/index.js
@@ -1,60 +1,3 @@
-var boxMullerRandom = (function () {
-    var phase = 0,
-        RAND_MAX,
-        array,
-        random,
-        x1, x2, w, z;
-
-    if (crypto && typeof crypto.getRandomValues === 'function') {
-        RAND_MAX = Math.pow(2, 32) - 1;
-        array = new Uint32Array(1);
-        random = function () {
-            crypto.getRandomValues(array);
-
-            return array[0] / RAND_MAX;
-        };
-    } else {
-        random = Math.random;
-    }
-
-    return function () {
-        if (!phase) {
-            do {
-                x1 = 2.0 * random() - 1.0;
-                x2 = 2.0 * random() - 1.0;
-                w = x1 * x1 + x2 * x2;
-            } while (w >= 1.0);
-
-            w = Math.sqrt((-2.0 * Math.log(w)) / w);
-            z = x1 * w;
-        } else {
-            z = x2 * w;
-        }
-
-        phase ^= 1;
-
-        return z;
-    }
-}());
-
-function randomWalk(steps, randFunc) {
-    steps = steps >>> 0 || 100;
-    var arr = Array.apply(0, Array(steps));
-    arr[0] = 0;
-    for (var t = 1; t < steps; t += 1) {
-        arr[t] = arr[t-1] + randFunc();
-    }
-    // console.log(arr.reduce(sum_reducer));
-    return arr;
-}
-
-var next_value = (value) => value + 2 * Math.random() - 1.0;
-// const sum_reducer = (accumulator, currentValue) => accumulator + currentValue;
-
-function myWalk() {
-	return 2 * Math.random() - 1.0;
-}
-
 var last_data = {};
 function ticker_recieved(data) {
   last_data = data;
@@ -71,7 +14,7 @@ var ticker_refresh = () => {
     $.support.cors = true;
   });
 }
-var ticker_interval = 500;
+var ticker_interval = 500; // milliseconds between ticker updates
 
 var chart; // integration point
 function requestData() {
@@ -121,24 +64,5 @@ $(document).ready(() => {
       series: [{ name: 'x', data: [] }]
   });
 });
-
-// $('#container').highcharts({
-//     title: { text: 'Value', x: -20 }, //center
-//     xAxis: { type: 'linear' },
-//     tooltip: {
-//         valueSuffix: 'x',
-//         pointFormat: '{point.y:.2f}',
-//     },
-//     legend: {
-//         layout: 'vertical',
-//         align: 'right',
-//         verticalAlign: 'middle',
-//         borderWidth: 0
-//     },
-//     series: [{
-//         name: 'x',
-//         data: randomWalk(100, boxMullerRandom), // myWalk),
-//     }],
-// });
 
 console.log('index.js loaded');
